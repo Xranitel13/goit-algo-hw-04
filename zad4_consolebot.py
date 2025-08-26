bf4d3c626dfd11edf5085c7add478136b55708bb
Xranitel13 <134066468+Xranitel13@users.noreply.github.com>
2025-08-26 17:01:25 +0200

    update2608

--- zad4_consolebot.py
+++ zad4_consolebot.py
@@ -1,50 +1,51 @@
 import sys
 
 contacts = []
-def add_contact(name: str, num: int):
+def add_contact(name: str, num: int)->str:
     for contact in contacts:
         if contact["number"] == num:
-            print(f"Contact with phone {contact['number']} already exists ( {contact['name']} )")
-            return
+            return f"Contact with phone [{contact['number']}] already exists ( {contact['name']} )"
+
         if contact["name"] == name:
-            print(f"Contact with name {contact['name']} already exists ({contact['number']} )")
-            return
+            return f"Contact [{contact['name']}] already exists ({contact['number']} )"
+
     contacts.append({"name": name, "number": num})
-    print(f"Contact {name} added successfully with number:{num} ")
+    return f"Contact [{name}] added successfully with number:{num} "
 
-
-
-def change_contact(name: str, num: int):
+def change_contact(name: str, num: int) -> str:
     for contact in contacts:
         if contact["name"] == name:
             contact["number"] = num
-            print(f"Contact {contact['name']} changed to {contact['number']} ")
-            return
-    print(f"Contact with name {name} does not exist")
+            return f"Number if contact [{contact['name']}] changed to {contact['number']} "
 
-def show_phone(name:str):
+    return f"Contact [{name}] does not exist"
+
+def show_phone(name:str) -> str:
     for contact in contacts:
         if contact["name"] == name:
-            print(f"{name}'s number is {contact["number"]}")
-            return
-    print(f"Contact with name {name} does not exist")
+            return f"[{name}] number is {contact["number"]}"
+
+    return f"Contact [{name}] does not exist"
 
 def show_all():
-    print("---name----|---number---")
+    text = "---name----|---number---\n"
     for contact in contacts:
-        print(contact["name"], contact["number"],sep="   :   ")
+        t = f"{contact["name"]}   :   {contact["number"]}\n"
+        text = text + t
+    return text
 
+def run_command(p:str,args:list[str]) -> str:
+    if not args:
+        return "Arguments wasn't find"
 
-def run_command(p:str,args:list[str]):
-    if len(args)>1 and not args[1].isdigit():
-        print("Wrong number format")
-        return
-    if p == "add":
-        add_contact(args[0], int(args[1]))
-    elif p == "change":
-        change_contact(args[0], int(args[1]))
+    if p == "add" and len(args) > 1 and args[1].isdigit():
+        return add_contact(args[0], int(args[1]))
+    elif p == "change" and len(args) > 1 and args[1].isdigit():
+        return change_contact(args[0], int(args[1]))
     elif p == "phone":
-        show_phone(args[0])
+        return show_phone(args[0])
+    else:
+        return "Wrong arguments of command"
 
 
 def main():
@@ -59,29 +60,13 @@ def main():
         elif p == "hello":
             print("How can I help you?")
         elif p == "add" or p == "change" or p == "phone":
-            if not args or len(args)<1 or len(args)<2 and (p == "add" or p == "change"):
-                print("Wrong arguments")
-            else:
-                run_command(p, args)
+                print(run_command(p, args))
         elif p == "all":
-            show_all()
+            print(show_all())
         else:
             print("Invalid command")
 
 
 
-
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
-
